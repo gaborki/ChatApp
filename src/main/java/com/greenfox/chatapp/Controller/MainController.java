bb1b5f907304471c8d2e5fa74b3c9a27fcfb2278
Gabor Kiszler <gabor.kiszler@gmail.com>
2017-05-17 12:20:09 +0200

    timestamp and method is souted to the console

--- src/main/java/com/greenfox/chatapp/Controller/MainController.java
+++ src/main/java/com/greenfox/chatapp/Controller/MainController.java
@@ -5,6 +5,7 @@ import com.greenfox.chatapp.Repository.LogRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MainController {
@@ -12,9 +13,9 @@ public class MainController {
   @Autowired
   LogRepo logRepo;
 
-  @RequestMapping("/")
+  @RequestMapping(value = "/")
   public String indexPage(){
-    logRepo.save(new Log());
+    logRepo.save(new Log("/", "GET"));
     return "index";
   }
 
--- src/main/java/com/greenfox/chatapp/Module/Log.java
+++ src/main/java/com/greenfox/chatapp/Module/Log.java
@@ -2,10 +2,13 @@ package com.greenfox.chatapp.Module;
 
 
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.xml.bind.SchemaOutputResolver;
 
 @Entity
 public class Log {
@@ -15,16 +18,36 @@ public class Log {
   private long id;
   private String path;
   private String method;
-  private Timestamp timestamp;
+  private String timestamp;
   private String logLevel;
   private String requestData;
 
   public Log(){
     this.method = "method";
     this.path = "path";
-    this.timestamp = new Timestamp(System.currentTimeMillis());
+    this.timestamp = getTime();
     this.logLevel = "INFO";
     this.requestData = "nothing";
+    printLog();
+  }
+
+  public Log(String endpoint, String method){
+    this.method = method;
+    this.path = endpoint;
+    this.timestamp = getTime();
+    this.logLevel = "INFO";
+    this.requestData = "nothing";
+    printLog();
+  }
+
+  public void printLog(){
+    System.out.println(this.timestamp);
+  }
+
+  public String getTime(){
+    LocalDateTime now = LocalDateTime.now();
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH-mm-ss");
+    return now.format(formatter);
   }
 
   public String getPath() {
@@ -43,11 +66,19 @@ public class Log {
     this.method = method;
   }
 
-  public Timestamp getTimestamp() {
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getTimestamp() {
     return timestamp;
   }
 
-  public void setTimestamp(Timestamp timestamp) {
+  public void setTimestamp(String timestamp) {
     this.timestamp = timestamp;
   }
 
