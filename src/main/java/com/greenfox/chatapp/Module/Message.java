6efe2c0ae515671d56badcadb8d50d1afffa53a1
Gabor Kiszler <gabor.kiszler@gmail.com>
2017-05-24 16:31:31 +0200

    update...

--- src/main/java/com/greenfox/chatapp/Module/Message.java
+++ src/main/java/com/greenfox/chatapp/Module/Message.java
@@ -3,8 +3,12 @@ package com.greenfox.chatapp.Module;
 import java.sql.Timestamp;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import org.springframework.stereotype.Component;
+import jdk.nashorn.internal.objects.annotations.Getter;
+import jdk.nashorn.internal.objects.annotations.Setter;
 
+
+@Setter
+@Getter
 @Entity
 public class Message {
 
@@ -38,38 +42,6 @@ public class Message {
     this.id = id;
     this.username = userName;
     this.text = text;
-    this.timestamp = timestamp;
-  }
-
-  public long getId() {
-    return id;
-  }
-
-  public void setId(long id) {
-    this.id = id;
-  }
-
-  public String getUsername() {
-    return username;
-  }
-
-  public void setUsername(String username) {
-    this.username = username;
-  }
-
-  public String getText() {
-    return text;
-  }
-
-  public void setText(String text) {
-    this.text = text;
-  }
-
-  public Timestamp getTimestamp() {
-    return timestamp;
-  }
-
-  public void setTimestamp(Timestamp timestamp) {
     this.timestamp = timestamp;
   }
 }
